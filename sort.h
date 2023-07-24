c34b0b4a4ec66b0f4bbda118160a56356b895c67
Diana Sungu <sungudiana1@gmail.com>
2023-07-24 05:15:53 +0300

    Header file

---
+++ sort.h
@@ -0,0 +1,43 @@
+#ifndef SORT_H
+#define SORT_H
+
+
+#define UP 0
+#define DOWN 1
+/**
+ * enum bool - boolean
+ * @true: true
+ * @false: false
+ */
+typedef enum bool
+{
+false = 0,
+true
+} bool;
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+void swap_ints(int *a, int *b);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+#endif
